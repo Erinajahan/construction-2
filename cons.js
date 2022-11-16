6198f6c75820753d21bcd2f056e00c1a9ad7ef8a
Erinajahan <erinaajahan@gmail.com>
2022-11-16 07:49:16 -0900

    first commit

---
+++ cons.js
@@ -0,0 +1,8 @@
+//swiper slider 1 
+var swiper = new Swiper(".mySwiper", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+  //swiper slider 1 end
